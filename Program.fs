ccd5ce4e3148ba8cf74ef77ffa84a3d66528c53f
QEES <546166132@qq.com>
2022-09-27 11:34:46 +0800

    update

--- Program.fs
+++ Program.fs
@@ -478,3 +478,37 @@ let m (a: A) =
     | :? B -> printfn "It's a B"
     | :? C -> printfn "It's a C"
     | _ -> ()
+
+let (|Even|Odd|) input1 = 
+    if (input1 % 2) = 0 
+    then Even 
+    else Odd
+   
+let TestNumber input =
+    match input with
+    | Even -> printfn "%d is even" input
+    | Odd -> printfn "%d is odd" input
+
+TestNumber 7
+TestNumber 11
+TestNumber 12
+
+//Partial Active Patterns
+let (|Integer|_|) (str:string) =
+    let mutable intvalue = 0
+    if System.Int32.TryParse(str, &intvalue) then Some(intvalue)
+    else None
+
+let (|Float|_|) (str:string) =
+    let mutable floatvalue = 0.0
+    if System.Double.TryParse(str, &floatvalue) then Some(floatvalue)
+    else None
+
+let parseNumeric str =
+    match str with
+    | Integer i -> printfn "%d : Integer" i
+    | Float f -> printfn "%f : Floating point" f
+    | _ -> printfn "%s : Not matched." str
+
+parseNumeric "1.1"
+parseNumeric "2"
