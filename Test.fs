d914bb0c86eadef4292afdb1194d89b8bcd15690
QEES <546166132@qq.com>
2022-10-07 23:36:50 +0800

    update

--- Test.fs
+++ Test.fs
@@ -3,4 +3,14 @@
 open System
 open System.Collections.Generic
 
-let a1 = 1
+//F#中的Measure
+[<Measure>] type 元
+[<Measure>] type 小孩
+[<Measure>] type 大人
+
+let kidPrice = 3<元/小孩>
+let adultPrice = 5<元/大人>
+
+let familyCost2 (child: int<小孩>) (adult: int<大人>) =
+    let result = kidPrice * child + adultPrice * adult
+    result
